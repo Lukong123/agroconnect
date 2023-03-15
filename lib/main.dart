201c7530e39cc0d2ff7f581d5b9784f8ddfb8e73
njein <njeinicodona@gmail.com>
2023-03-15 04:53:13 +0100

    made some changes

--- lib/main.dart
+++ lib/main.dart
@@ -16,25 +16,3 @@ void main() {
   ));
 
 }
-
-
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
-  void main() => runApp(const MyApp());
- 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
- 
- 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Login(),
-      ),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
- 
